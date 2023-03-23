093a9d575975515ca036d10a394d0ea27cc19b09
sipra <sipramishra085@gmail.com>
2023-03-23 18:01:50 +0530

    Added program main file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AddressBookProblem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("!!!!!!Welcome to Adress Book Program!!!!!!");
+            AddPerson contact = new AddPerson();
+            contact.details();
+        }
+    }
+}
