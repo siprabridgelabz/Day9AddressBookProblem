c6ecb7ee32b71bd98da3e8e7bed77a20fd01dab4
sipra <sipramishra085@gmail.com>
2023-03-23 18:06:33 +0530

    Added UC3

---
+++ contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProblem
+{
+    public class contact
+    {
+        
+        
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+
+        public int zip { get; set; }
+        public int phoneNumber { get; set; }
+        public string eMail { get; set; }
+
+
+    }
+}
+
